cd37d96a4ed93a016ecc36b4f9f4cb263272c56e
Cody <lawdermiltcody@gmail.com>
2020-02-06 11:06:12 -0600

    installed bootstrap and added some few components

---
+++ src/components/CustomNav.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Col, Row } from "react-bootstrap";
+
+class CustomNav extends Component {
+  render() {
+    return (
+      <nav>
+        <Navbar bg="dark">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <div className="nav-detail">
+            <Row>
+              <Col>
+                <Link to="/">Home</Link>
+              </Col>
+
+              <Col>
+                <Link to="/about">About</Link>
+              </Col>
+
+              <Col>
+                <Link to="/projects">Projects</Link>
+              </Col>
+            </Row>
+          </div>
+        </Navbar>
+      </nav>
+    );
+  }
+}
+
+export default CustomNav;
