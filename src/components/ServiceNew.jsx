b1685baf1a30139131472ee6ec0bd9b7c96f3285
Cody <lawdermiltcody@gmail.com>
2020-05-16 17:01:18 -0500

    be able to delete a service

--- src/components/ServiceNew.jsx
+++ src/components/ServiceNew.jsx
@@ -1,27 +1,42 @@
 import React, { Component } from "react";
-import { Form } from "semantic-ui-react";
+import { Form, List, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 class ServiceNew extends Component {
   render() {
+    const { services } = this.props;
     return (
-      <Form onSubmit={this.props.addingNewService}>
-        <Form.Group widths="equal">
-          <Form.Input
-            fluid
-            label="Service Name"
-            placeholder="Service Name"
-            id="name"
-            name="name"
-            value={this.props.ServiceNew}
-            onChange={this.props.handleChangeServiceNew}
-          />
-          <button class="ui button">Submit</button>
-          <Link to="/client">
-            <button className="back-button ui button">Back</button>
-          </Link>
-        </Form.Group>
-      </Form>
+      <div>
+        <Form onSubmit={this.props.addingNewService}>
+          <Form.Group widths="equal">
+            <Form.Input
+              fluid
+              label="Service Name"
+              placeholder="Service Name"
+              id="name"
+              name="name"
+              value={this.props.ServiceNew}
+              onChange={this.props.handleChangeServiceNew}
+            />
+            <button class="ui button">Submit</button>
+            <Link to="/client">
+              <button className="back-button ui button">Back</button>
+            </Link>
+          </Form.Group>
+        </Form>
+        {services.length > 0 &&
+          services.map((service) => (
+            <List>
+              <List.Item>{service.name}</List.Item>
+              <Button
+                color="red"
+                onClick={() => this.props.serviceOnClickDelete(service)}
+              >
+                Delete
+              </Button>
+            </List>
+          ))}
+      </div>
     );
   }
 }
