201c6ee02a151fca04a6b677b2f1f1b26a8ab139
Cody <lawdermiltcody@gmail.com>
2020-06-02 20:49:51 -0500

    be able to update pet with date

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -4,21 +4,23 @@ import { Card, Button } from "semantic-ui-react";
 class History extends Component {
   render() {
     const { pets, services } = this.props;
-    const date = [];
+    const data = [];
 
     if (pets.length > 0) {
       pets.map((pet) => {
-        console.log(pet.name);
         if (pet.appointments.length > 0) {
-          pet.appointments.map((a) => {
-            data.push(a);
+          pet.appointments.map((appointment) => {
+            pet.services.map((service) => {
+              data.push({
+                service: appointment.date,
+              });
+            });
           });
         }
       });
     }
 
-    console.log(date);
-
+    console.log(data);
     return (
       <Card.Group>
         {this.props.pets.length > 0 ? (
@@ -26,12 +28,18 @@ class History extends Component {
             <Card>
               <Card.Content>
                 <Card.Header>{pet.name}</Card.Header>
-                {pet.appointments.map((info) => (
-                  <Card.Meta>Date {info.date}</Card.Meta>
-                ))}
-                {pet.services.map((info) => (
-                  <Card.Meta>Date {info.name}</Card.Meta>
-                ))}
+                <div>
+                  {pet.appointments.map((info) => (
+                    <div>
+                      {pet.services.map((s) => (
+                        <div>
+                          <Card.Meta>Date {info.date}</Card.Meta>
+                          <Card.Meta>Service: {s.name}</Card.Meta>
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
               </Card.Content>
               <Card.Content extra>
                 <div className="ui two buttons">
